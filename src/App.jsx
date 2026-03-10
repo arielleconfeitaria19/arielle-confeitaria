2f840e03da25de5545836e1925394b2a40bc047a
arielleconfeitaria19 <arielleconfeitaria@gmail.com>
2026-03-10 16:51:25 -0300

    fix rota admin vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
@@ -29,7 +29,7 @@ export default function App() {
   }
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
 
@@ -40,6 +40,6 @@ export default function App() {
           element={usuario ? <Admin /> : <Navigate to="/login" />}
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
