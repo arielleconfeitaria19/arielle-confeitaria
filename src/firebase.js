65972e67062d41f6b87f4964bd84b0e5d3c12eeb
Helio de Souza <heliodesouza@gmail.com>
2026-03-12 21:58:59 -0300

    update

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyA2wJAidBQMmyEki90Ur5NivLobh8lxu1Y",
+  authDomain: "arielle-confeitaria-c8282.firebaseapp.com",
+  projectId: "arielle-confeitaria-c8282",
+  storageBucket: "arielle-confeitaria-c8282.firebasestorage.app",
+  messagingSenderId: "619669394494",
+  appId: "1:619669394494:web:a837d07c20c16333536d6f"
 };
 
 
